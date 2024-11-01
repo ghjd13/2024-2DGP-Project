8ae0d5113a1226a689a7a52857cb1940b1bf26e4
ghjd13 <69751035+ghjd13@users.noreply.github.com>
2024-11-01 10:27:14 +0900

    10.31
    속도에 따라 그림이 바뀌는 함수 구현

--- game/background.py
+++ game/background.py
@@ -23,9 +23,15 @@ class Background(gfw.Sprite):
         self.dspeed = 0
         self.width = self.image.w * Background.IMAGE_MAG
         self.height = self.image.h * Background.IMAGE_MAG
-        self.min_x = 0
-        self.max_x = get_canvas_width()
+        self.min_x = get_canvas_width() // 4
+        self.max_x = (get_canvas_width() // 4)*3
         self.roll_time = 0
+
+        # 이미지 시리즈 로드
+        self.images = [load_image(f'res/road{i}.png') for i in range(1, 5)]
+        self.current_image_index = 0
+        self.image_change_speed = 1  # 이미지 변경 속도
+        self.frame_count = 0  # 프레임 카운트 초기화
 
     def handle_event(self, e):
         pair = (e.type, e.key)
@@ -36,15 +42,37 @@ class Background(gfw.Sprite):
         print(self.speed, self.dspeed)
 
     def update(self):
+        if self.x <= self.min_x or self.x >= self.max_x:
+            self.dspeed = -1
+        elif self.speed < 0:
+            self.speed = 0
+            self.dspeed = 0
+        elif self.dspeed == -1:
+            self.dspeed = 0
         self.speed += self.dspeed * gfw.frame_time *10
         self.x += self.dx * self.speed * gfw.frame_time
+        self.x = clamp(self.min_x, self.x, self.max_x)
+
+
+
         self.x = self.x % self.width  # 배경이 반복되도록 설정
         self.update_roll()
 
+        # 이미지 변경 속도 조절
+        if self.speed == 0:
+            self.image_change_speed = 0
+        else:
+            self.image_change_speed = max(1, int(200 / (self.speed + 1)))
+
+        self.frame_count += 1
+        if self.image_change_speed > 0 and self.frame_count % self.image_change_speed == 0:
+            self.current_image_index = (self.current_image_index + 1) % len(self.images)
+
     def draw(self):
-        self.image.draw(self.x, self.y)
+        current_image = self.images[self.current_image_index]
+        current_image.draw(self.x, self.y)
         # 배경이 반복되도록 두 번째 이미지를 그리기
-        self.image.draw(self.x - self.width, self.y)
+        current_image.draw(self.x - self.width, self.y)
 
     def update_roll(self):
         roll_dir = self.dx
