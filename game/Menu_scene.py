a00036dd3d2db322e3cac485d5cd798ac7230c73
ghjd13 <69751035+ghjd13@users.noreply.github.com>
2024-12-12 14:30:38 +0900

    버튼 제작
    누르면 이벤트가 발생하게 제작

--- game/Menu_scene.py
+++ game/Menu_scene.py
@@ -12,8 +12,10 @@ world = World(2)
 
 def enter():
     world.append(HorzFillBackground('res/UI/menuScreen.png'), 0)
-    world.append(Sprite('res/UI/GameStartButton.png', 100, 200), 0)
-    world.append(Sprite('res/UI/GameDescriptionButton.png', 100, 75), 0)
+    startButton = Sprite('res/UI/GameStartButton.png', 100, 200)
+    world.append(startButton, 0)
+    descriptionButton = Sprite('res/UI/GameDescriptionButton.png', 100, 75)
+    world.append(descriptionButton, 0)
 
     import json
 
@@ -29,11 +31,13 @@ def draw():
     pass
 
 def handle_event(e):
+    if e.type == SDL_MOUSEBUTTONDOWN:
+        mx, my = e.x, e.y
+        if mx > 37 and mx < 163 and my > 217 and my <343:
+            gfw.push(main_scene)
+        elif mx > 37 and mx < 163 and my > 343 and my <468:
+            gfw.push(main_scene)
     if e.type == SDL_KEYDOWN:
-        if e.key == SDLK_UP:
-            pass
-        if e.key == SDLK_DOWN:
-            pass
         if e.key == SDLK_RETURN:
             gfw.push(main_scene)
 
